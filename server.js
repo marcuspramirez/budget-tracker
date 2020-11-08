91877fc264ca1e09ce75b99a9a589707c15062d7
Marcus Ramirez <macusramirez@gmail.com>
2020-11-08 17:25:16 -0600

    fixed dirctory

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const path = require("path");
 
 
 const PORT = process.env.PORT || 3000;
@@ -29,9 +28,7 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api.js"));
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
