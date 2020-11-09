d0a781a2b480509a6c115cba798fab7634120c43
Marcus Ramirez <macusramirez@gmail.com>
2020-11-09 14:33:37 -0600

    fixed manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,9 @@
 console.log("Hello from service worker!")
+
+var preLoad = function() {
+    console.log("Installing web app");
+    return caches.open("offline").then(function(cache) {
+        console.log("caching index and important routes");
+        return cache.addAll(["/"]);
+    });
+};
